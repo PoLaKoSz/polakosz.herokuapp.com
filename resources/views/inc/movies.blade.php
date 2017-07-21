5f459471054cacaf2c4850444d248b40348d0e90
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2017-07-21 16:32:43 +0200

    Home page Movies section now get data from DB

--- resources/views/inc/movies.blade.php
+++ resources/views/inc/movies.blade.php
@@ -10,106 +10,23 @@
             </div>
         </div>
 
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="row">
-                <div class="view overlay hm-black-strong">
-                    <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
-                    <div class="mask flex-center">
-                        <div>
-                            <p class="white-text">I recommend</p>
+        @foreach($movies as $movie)
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                <div class="row">
+                    <div class="view overlay hm-black-strong">
+                        <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
+                        <div class="mask flex-center">
+                            <div>
+                                <p class="white-text">{{ $movie->csillag }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="row text-center">
-                <h4><a href="#">Ghost in the Shell</a></h4>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="row">
-                <div class="view overlay hm-black-strong">
-                    <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
-                    <div class="mask flex-center">
-                        <div>
-                            <p class="white-text">Only once</p>
-                        </div>
-                    </div>
+                <div class="row text-center">
+                    <h4><a href="#">{{ $movie->filmcim }}</a></h4>
                 </div>
             </div>
-
-            <div class="row text-center">
-                <h4><a href="#">Original Title</a></h4>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="row">
-                <div class="view overlay hm-black-strong">
-                    <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
-                    <div class="mask flex-center">
-                        <div>
-                            <p class="white-text">Not so good</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row text-center">
-                <h4><a href="#">Original Title</a></h4>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="row">
-                <div class="view overlay hm-black-strong">
-                    <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
-                    <div class="mask flex-center">
-                        <div>
-                            <p class="white-text">OMG, this is shit</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row text-center">
-                <h4><a href="#">Original Title</a></h4>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="row">
-                <div class="view overlay hm-black-strong">
-                    <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
-                    <div class="mask flex-center">
-                        <div>
-                            <p class="white-text">I will kill myself so shit</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row text-center">
-                <h4><a href="#">Original Title</a></h4>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="row">
-                <div class="view overlay hm-black-strong">
-                    <img src="{{asset('images/imagenotfound.svg')}}" alt="All projects">
-                    <div class="mask flex-center">
-                        <div>
-                            <p class="white-text">Doomsday</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row text-center">
-                <h4><a href="#">Original Title</a></h4>
-            </div>
-        </div>
+        @endforeach
     </div>
 </section>
