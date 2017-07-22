16d0cf606797b97252e871815d3617f20d12e0a8
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2017-07-22 20:12:40 +0200

    Welcome section margin changed

--- resources/views/inc/welcome_text.blade.php
+++ resources/views/inc/welcome_text.blade.php
@@ -1,5 +1,5 @@
-<section id="welcome-text">
-    <div>
+<section id="welcome-text" class="">
+    <div class="container">
         <h1 class="text-center giant-text row">{{ trans('welcome_text.big_text_simple') }} <span class="primary-text">{{ trans('welcome_text.big_text_highlighted') }}</span>!</h1>
         <h3 class="secondary-text text-center italic-text row big-text">{{ trans('welcome_text.small_text') }}</h3>
     </div>
