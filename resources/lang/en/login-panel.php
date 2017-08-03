a4bdeb20f1e1ff633dcfb78dad50a42eb77b9ad8
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2017-08-03 08:40:50 +0200

    Login panel translated

---
+++ resources/lang/en/login-panel.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Loginpanel Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | resource\views\inc\login.blade.php
+    | resource\views\inc\register.blade.php
+    |
+    */
+
+    'name'            => '(Nick)name',
+    'email'           => 'E-Mail Address',
+    'password'        => 'Password',
+    'password_conf'   => 'Confirm Password',
+    'remember_me'     => 'Remember Me',
+    
+    'btn_login'       => 'Login',
+    'btn_reg'         => 'Registration',
+
+    'forgot_password' => 'Forgot Your Password?',
+
+    'reg_disabled'    => 'The registration currently disabled.',
+
+];
--- resources/views/inc/register.blade.php
+++ resources/views/inc/register.blade.php
@@ -3,7 +3,7 @@
         {{ csrf_field() }}
 
         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Name</label>
+            <label for="name" class="col-md-4 control-label">{{ trans('login-panel.name') }}</label>
 
             <div class="col-md-6">
                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
@@ -17,7 +17,7 @@
         </div>
 
         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+            <label for="email" class="col-md-4 control-label">{{ trans('login-panel.email') }}</label>
 
             <div class="col-md-6">
                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -31,7 +31,7 @@
         </div>
 
         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <label for="password" class="col-md-4 control-label">Password</label>
+            <label for="password" class="col-md-4 control-label">{{ trans('login-panel.password') }}</label>
 
             <div class="col-md-6">
                 <input id="password" type="password" class="form-control" name="password" required>
@@ -45,7 +45,7 @@
         </div>
 
         <div class="form-group">
-            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+            <label for="password-confirm" class="col-md-4 control-label">{{ trans('login-panel.password_conf') }}</label>
 
             <div class="col-md-6">
                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -55,11 +55,11 @@
         <div class="form-group">
             <div class="col-md-6 col-md-offset-4">
                 <button type="submit" class="btn btn-primary">
-                    Register
+                    {{ trans('login-panel.btn_reg') }}
                 </button>
             </div>
         </div>
     </form>
 @else
-    <p class="alert alert-info">A regisztráció jelenleg le van zárva.</p>
+    <p class="alert alert-info">{{ trans('login-panel.reg_disabled') }}</p>
 @endif
