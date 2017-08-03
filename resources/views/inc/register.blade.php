2ba0403d24a7c7135a1db6c3144e0e5fd23a39d7
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2017-08-03 08:51:56 +0200

    Register panel labels fixed

--- resources/views/inc/register.blade.php
+++ resources/views/inc/register.blade.php
@@ -16,39 +16,39 @@
             </div>
         </div>
 
-        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">{{ trans('login-panel.email') }}</label>
+        <div class="form-group{{ $errors->has('reg_email') ? ' has-error' : '' }}">
+            <label for="reg_email" class="col-md-4 control-label">{{ trans('login-panel.email') }}</label>
 
             <div class="col-md-6">
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                <input id="reg_email" type="reg_email" class="form-control" name="reg_email" value="{{ old('reg_email') }}" required>
 
-                @if ($errors->has('email'))
+                @if ($errors->has('reg_email'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
+                        <strong>{{ $errors->first('reg_email') }}</strong>
                     </span>
                 @endif
             </div>
         </div>
 
-        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <label for="password" class="col-md-4 control-label">{{ trans('login-panel.password') }}</label>
+        <div class="form-group{{ $errors->has('reg_password') ? ' has-error' : '' }}">
+            <label for="reg_password" class="col-md-4 control-label">{{ trans('login-panel.password') }}</label>
 
             <div class="col-md-6">
-                <input id="password" type="password" class="form-control" name="password" required>
+                <input id="reg_password" type="reg_password" class="form-control" name="reg_password" required>
 
-                @if ($errors->has('password'))
+                @if ($errors->has('reg_password'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
+                        <strong>{{ $errors->first('reg_password') }}</strong>
                     </span>
                 @endif
             </div>
         </div>
 
         <div class="form-group">
-            <label for="password-confirm" class="col-md-4 control-label">{{ trans('login-panel.password_conf') }}</label>
+            <label for="reg_password-confirm" class="col-md-4 control-label">{{ trans('login-panel.password_conf') }}</label>
 
             <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                <input id="reg_password-confirm" type="reg_password" class="form-control" name="reg_password_confirmation" required>
             </div>
         </div>
 
